6211a7410796f3da67cc42314a8ce286de27eb5a
Isaac Miti <ikayz@live.com>
2022-03-14 16:12:41 +0200

    Set the resource id and string id in the recycler view

--- app/src/main/java/com/ikayz/kotlin/famousquotes/adapter/ItemAdapter.kt
+++ app/src/main/java/com/ikayz/kotlin/famousquotes/adapter/ItemAdapter.kt
@@ -1,28 +1,36 @@
 package com.ikayz.kotlin.famousquotes.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.ads.mediationtestsuite.viewmodels.ItemViewHolder
+import com.ikayz.kotlin.famousquotes.R
 import com.ikayz.kotlin.famousquotes.model.Quotes
 
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Quotes>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ItemViewHolder {
-        TODO("Not yet implemented")
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textview_item)
+        val imageView: ImageView = view.findViewById(R.id.imageview_item)
     }
 
-    override fun onBindViewHolder(holder: ItemAdapter.ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+        return ItemViewHolder(adapterLayout)
     }
 
-    override fun onBindViewHolder(
-        holder: ItemAdapter.ItemViewHolder,
-        position: Int,
-        payloads: MutableList<Any>
-    ) {
-        super.onBindViewHolder(holder, position, payloads)
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
+
+    override fun getItemCount() = dataset.size
 }
