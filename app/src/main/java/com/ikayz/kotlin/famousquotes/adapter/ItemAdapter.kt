145bdd7b85045c154735ba48d495e77ea6eea4a3
Isaac Miti <ikayz@live.com>
2022-03-14 14:39:54 +0200

    Add recycker view

--- app/src/main/java/com/ikayz/kotlin/famousquotes/adapter/ItemAdapter.kt
+++ app/src/main/java/com/ikayz/kotlin/famousquotes/adapter/ItemAdapter.kt
@@ -1,4 +1,11 @@
 package com.ikayz.kotlin.famousquotes.adapter
 
-class ItemAdapter {
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.ikayz.kotlin.famousquotes.model.Quotes
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Quotes>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 }
--- app/src/main/java/com/ikayz/kotlin/famousquotes/model/Quotes.kt
+++ app/src/main/java/com/ikayz/kotlin/famousquotes/model/Quotes.kt
@@ -1,4 +1,7 @@
 package com.ikayz.kotlin.famousquotes.model
 
-class Quotes {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Quotes(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
 }
