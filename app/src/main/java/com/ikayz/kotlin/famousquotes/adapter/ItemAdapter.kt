3386a7e2fca11d208fad997feffd1ba223e1ae1c
Isaac Miti <ikayz@live.com>
2022-03-14 15:09:52 +0200

    Import image drawables

--- app/src/main/java/com/ikayz/kotlin/famousquotes/adapter/ItemAdapter.kt
+++ app/src/main/java/com/ikayz/kotlin/famousquotes/adapter/ItemAdapter.kt
@@ -1,11 +1,28 @@
 package com.ikayz.kotlin.famousquotes.adapter
 
 import android.content.Context
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.ads.mediationtestsuite.viewmodels.ItemViewHolder
 import com.ikayz.kotlin.famousquotes.model.Quotes
 
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Quotes>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemAdapter.ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: ItemAdapter.ItemViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        super.onBindViewHolder(holder, position, payloads)
+    }
 }
--- app/src/main/java/com/ikayz/kotlin/famousquotes/data/Datasource.kt
+++ app/src/main/java/com/ikayz/kotlin/famousquotes/data/Datasource.kt
@@ -1,4 +1,11 @@
 package com.ikayz.kotlin.famousquotes.data
 
+import com.ikayz.kotlin.famousquotes.model.Quotes
+
 class Datasource {
+    fun loadQuotes(): List<Quotes> {
+        return listOf<Quotes>(
+            Quotes()
+        )
+    }
 }
